fcab73e3effc6a72cfb62d17f01bf43dea8a5543
jooyounghan0823 <153965200+jooyounghan0823@users.noreply.github.com>
2023-12-17 13:47:56 +0900

    Add files via upload

---
+++ Day2/bool_list.py
@@ -0,0 +1,21 @@
+#doyoueatcub = false
+
+#istall = true
+#isfound = false
+#isoverthreekg = false
+
+#print(type(doyoueatcub))
+
+#정수를불리안 타입가능?
+#float()
+#bool()
+#int("123")
+
+#a = bool("") #false
+#b = bool(0)
+#c = bool(123456)
+
+#a = list("현우진은 일타강사") # 문자열을 리스트화
+#print(a)
+soccer = ["손흥민","황희찬"]
+print(soccer)
---
+++ Day2/quiz1.py
@@ -0,0 +1,12 @@
+#선생님 답지
+#year = int(input("태어난년도입력"))
+#print(f"(2023-year))
+
+#krw = int(input("원화 입력:"))
+#usd = 0.00077
+#yen = 0.11
+#print(f"달러: {krw*usd}, 엔:{krw*yen}")
+
+#km = int(input("km 입력:"))
+#mile = 0.62
+#print(f"{km*mile}입니다.")
