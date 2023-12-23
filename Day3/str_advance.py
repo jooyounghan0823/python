76d3255f7b5fb73c12f95437848357082fac9592
jooyounghan0823 <153965200+jooyounghan0823@users.noreply.github.com>
2023-12-23 13:49:43 +0900

    Add files via upload

---
+++ Day3/str_advance.py
@@ -0,0 +1,61 @@
+#3)인덱스(색인)
+#coffee = "americano"
+#print(coffee[0])
+#print(coffee[-1])
+
+#4)슬라이싱
+#test = "pytyon"
+#print(test[1:3])
+
+#a = 'megastudy'
+#b = a.upper()
+#print(b)
+
+#x = mega.islower()
+#print(x)
+
+
+#3. 공백제거
+#marriage = " 오늘 결혼식 추카"
+#newMarriage = marrigae.strip()
+#print(newMarriage)
+#con = "오늘 결혼 축하해"
+#test = con.split()
+#print(test)
+
+#news = "The Supreme Court's decision not to rule quickly on whether Donald Trump can be prosecuted on election-subversion charges may help delay his trial, according to ex-prosecutors."
+#test = news.split()
+#print(test)
+
+
+
+#article = """The Supreme Court's decision not to rule quickly on whether Donald Trump can be prosecuted on election-subversion charges may help delay his trial, according to ex-prosecutors."""
+
+#newarticle = article.replace("Trump","HAN")
+#print(newarticle)
+
+
+#article = """The Supreme Court's decision not to rule quickly on whether Donald Trump can be prosecuted on election-subversion charges may help delay his trial, according to ex-prosecutors."""
+#location = article.find("rule")
+#print(location)
+
+
+#menu = ['아아','라떼','초라']
+#result = '존맛'.join(menu)
+#print(result)
+
+#article = """The Supreme Court's decision not to rule quickly on whether Donald Trump can be prosecuted on election-subversion charges may help delay his trial, according to ex-prosecutors."""
+#test = article.split()
+#print(test)
+#test1 = '꿀잼'.join(test)
+#print(test1)
+
+
+#news = article1.replce(' '+word+' ')
+
+#a = article1.count("축의금")
+#print(a)
+
+
+burger = ['버거킹','노브랜드','맘스터치','맥도날드']
+result = '롯데리아' in burger
